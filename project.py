517fe3e0accbb446e0ff92924430aea93b2514db
kniven51 <kaleb.niven51@gmail.com>
2018-07-08 22:20:53 -0500

    change newMenuItem to use flask menu.html template

--- project.py
+++ project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -19,14 +19,20 @@ def start_pate():
 def restaurantMenu(restaurant_id):
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    return render_template('menu.html' name=name)
+    return render_template('menu.html' restaurant=restaurant, items=items)
 
 # Task 1: Create route for newMenuItem function here
-
-
-@app.route('/restaurant/<int:restaurant_id>/new/')
+@app.route('/restaurant/<int:restaurant_id>/new/', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
-    return "page to create a new menu item. Task 1 complete!"
+    if request.method == 'POST':
+        newItem = MenuItem(
+            name=request.form['name'], restaurant_id=restaurant_id)
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('new_menu_item.html', restaurant_id=restarant_id)
+        
 
 # Task 2: Create route for editMenuItem function here
 
